131c61a8e60c0260fbcb7370b388b40a6c252310
kangboom <jkl6147@naver.com>
2025-07-23 16:32:54 +0900

    chore: initial commit

---
+++ ottereview/src/main/java/com/ssafy/ottereview/OttereviewApplication.java
@@ -0,0 +1,13 @@
+package com.ssafy.ottereview;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OttereviewApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(OttereviewApplication.class, args);
+	}
+
+}
---
+++ ottereview/src/test/java/com/ssafy/ottereview/OttereviewApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ssafy.ottereview;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OttereviewApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
