3c1a4ef124378610a505fe28a8bbd1cdb27c8cc1
윤경일 <kidreamx@naver.com>
2025-07-25 17:59:27 +0900

    [FEATURE] github app에서 사용자 repository 가져오기

--- ottereview/src/main/java/com/ssafy/ottereview/repo/dto/RepoCreateRequest.java
+++ ottereview/src/main/java/com/ssafy/ottereview/repo/dto/RepoCreateRequest.java
@@ -1,7 +1,10 @@
 package com.ssafy.ottereview.repo.dto;
 
+import java.util.Objects;
+import lombok.Builder;
 import lombok.Getter;
 
+@Builder
 @Getter
 public class RepoCreateRequest {
 
@@ -11,4 +14,18 @@ public class RepoCreateRequest {
 
     private boolean isPrivate;
 
+    private long installationId;
+
+    @Override
+    public boolean equals(Object o){
+        if (this == o) return true;                        // 1) 동일 참조 체크
+        if (o == null || getClass() != o.getClass()) return false;  // 2) 타입 체크\
+        RepoCreateRequest repoCR = (RepoCreateRequest) o;
+        return this.githubOwnerUsername.equals(repoCR.getGithubOwnerUsername())
+                && this.githubRepoName.equals(repoCR.getGithubRepoName());
+    }
+    @Override
+    public int hashCode() {
+        return Objects.hash(this.githubOwnerUsername, this.githubRepoName);
+    }
 }
--- ottereview/src/main/java/com/ssafy/ottereview/userreporelation/entity/UserRepoRelation.java
+++ ottereview/src/main/java/com/ssafy/ottereview/userreporelation/entity/UserRepoRelation.java
@@ -3,7 +3,6 @@ package com.ssafy.ottereview.userreporelation.entity;
 import com.ssafy.ottereview.repo.entity.Repo;
 import com.ssafy.ottereview.user.entity.User;
 import jakarta.persistence.Column;
-import jakarta.persistence.Embedded;
 import jakarta.persistence.Entity;
 import jakarta.persistence.FetchType;
 import jakarta.persistence.GeneratedValue;
@@ -11,14 +10,13 @@ import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
 import jakarta.persistence.JoinColumn;
 import jakarta.persistence.ManyToOne;
-import jakarta.persistence.MapsId;
 import jakarta.persistence.Table;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 
-@Entity
 @Getter
+@Entity
 @NoArgsConstructor
 @AllArgsConstructor
 @Table(name = "user_repository_relation")
