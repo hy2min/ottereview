1c33b251f0144ac810d577dfa3461f6916346b05
huizoo <ajtwlsgmlwn@naver.com>
2025-07-29 19:07:16 +0900

    chore: conflict 방지하기 위해 dev 변경사항 수동 반영

--- ottereview-fe/vite.config.js
+++ ottereview-fe/vite.config.js
@@ -1,8 +1,16 @@
+import { NodeGlobalsPolyfillPlugin } from '@esbuild-plugins/node-globals-polyfill'
 import tailwindcss from '@tailwindcss/vite'
 import react from '@vitejs/plugin-react-swc'
 import { defineConfig } from 'vite'
-
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react(), tailwindcss()],
+  optimizeDeps: {
+    esbuildOptions: {
+      define: {
+        global: 'globalThis',
+      },
+      plugins: [NodeGlobalsPolyfillPlugin({ buffer: true })],
+    },
+  },
 })
