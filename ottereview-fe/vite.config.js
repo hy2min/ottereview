4530d752448a47f792ee4897360328c2eeeb819a
이혜민 <hy1x1mn@gmail.com>
2025-07-29 14:36:37 +0900

    fix: SockJS 오류 해결을 위한 globalThis 및 NodeGlobalPolyfillPlugin 설정 추가

--- ottereview-fe/vite.config.js
+++ ottereview-fe/vite.config.js
@@ -1,3 +1,4 @@
+import { NodeGlobalsPolyfillPlugin } from '@esbuild-plugins/node-globals-polyfill'
 import tailwindcss from '@tailwindcss/vite'
 import react from '@vitejs/plugin-react-swc'
 import { defineConfig } from 'vite'
@@ -5,4 +6,12 @@ import { defineConfig } from 'vite'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react(), tailwindcss()],
+  optimizeDeps: {
+    esbuildOptions: {
+      define: {
+        global: 'globalThis',
+      },
+      plugins: [NodeGlobalsPolyfillPlugin({ buffer: true })],
+    },
+  },
 })
