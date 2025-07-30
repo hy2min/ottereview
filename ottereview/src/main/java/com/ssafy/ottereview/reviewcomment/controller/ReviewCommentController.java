87e11c6ecca008ac38f9c806c4eedd64c071876b
신진우 <sjw0066@naver.com>
2025-07-30 15:26:42 +0900

    [BE/FEATURE] 리뷰 코멘트 기능 CRUD구현, 일괄 추가 구현

--- ottereview/src/main/java/com/ssafy/ottereview/reviewcomment/controller/ReviewCommentController.java
+++ ottereview/src/main/java/com/ssafy/ottereview/reviewcomment/controller/ReviewCommentController.java
@@ -1,9 +1,7 @@
 package com.ssafy.ottereview.reviewcomment.controller;
 
-import com.ssafy.ottereview.reviewcomment.dto.ReviewCommentCreateRequest;
 import com.ssafy.ottereview.reviewcomment.dto.ReviewCommentResponse;
 import com.ssafy.ottereview.reviewcomment.dto.ReviewCommentUpdateRequest;
-import com.ssafy.ottereview.reviewcomment.entity.ReviewComment;
 import com.ssafy.ottereview.reviewcomment.service.ReviewCommentService;
 import com.ssafy.ottereview.user.entity.CustomUserDetail;
 import java.util.List;
@@ -15,41 +13,27 @@ import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RequestPart;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.multipart.MultipartFile;
 
 @RestController
-@RequestMapping("/api/pull-requests/{pr-id}/comments")
+@RequestMapping("/api/reviews/{review-id}/comments")
 @RequiredArgsConstructor
 @Slf4j
 public class ReviewCommentController {
 
     private final ReviewCommentService reviewCommentService;
 
-    @PostMapping
-    public ResponseEntity<?> createComment(
-            @PathVariable("pr-id") Long prId,
-            @ModelAttribute("comment") ReviewCommentCreateRequest request,
-            @RequestPart(value = "file", required = false) MultipartFile file,
-            @AuthenticationPrincipal CustomUserDetail userDetail) {
-
-
-        ReviewCommentResponse comment = reviewCommentService.createComment(prId, request, file, userDetail.getUser().getId());
-        return ResponseEntity.ok(comment);
-
-    }
-    
     @GetMapping
-    public ResponseEntity<?> getCommentsByPullRequest(
-            @PathVariable("pr-id") Long prId) {
+    public ResponseEntity<?> getCommentsByReview(
+            @PathVariable("review-id") Long reviewId) {
 
-            List<ReviewCommentResponse> comments = reviewCommentService.getCommentsByPullRequestId(prId);
-            return ResponseEntity.ok(comments);
-
+        List<ReviewCommentResponse> comments = reviewCommentService.getCommentsByReviewId(reviewId);
+        return ResponseEntity.ok(comments);
     }
 
 //    @PutMapping("/{comment-id}")
@@ -67,27 +51,45 @@ public class ReviewCommentController {
 
     @PutMapping("/{comment-id}")
     public ResponseEntity<?> updateComment(
-            @PathVariable("pr-id") Long prId,
+            @PathVariable("review-id") Long reviewId,
             @PathVariable("comment-id") Long commentId,
             @ModelAttribute ReviewCommentUpdateRequest request,
             @RequestPart(value = "file", required = false) MultipartFile file,
             @AuthenticationPrincipal CustomUserDetail userDetail) {
 
-        ReviewCommentResponse comment = reviewCommentService.updateComment(commentId, request, userDetail.getUser().getId(), file);
+        ReviewCommentResponse comment = reviewCommentService.updateComment(commentId, request,
+                userDetail.getUser().getId(), file);
         return ResponseEntity.ok(comment);
     }
 
     @DeleteMapping("/{comment-id}")
     public ResponseEntity<String> deleteComment(
-            @PathVariable("pr-id") Long prId,
+            @PathVariable("review-id") Long reviewId,
             @PathVariable("comment-id") Long commentId,
             @AuthenticationPrincipal CustomUserDetail userDetail) {
-        
 
         reviewCommentService.deleteComment(commentId, userDetail.getUser().getId());
         return ResponseEntity.ok("댓글이 성공적으로 삭제되었습니다.");
     }
-    
+
+    @GetMapping("/users/{user-id}")
+    public ResponseEntity<?> getCommentsByUser(
+            @PathVariable("user-id") Long userId) {
+
+        List<ReviewCommentResponse> comments = reviewCommentService.getCommentsByUserId(userId);
+        return ResponseEntity.ok(comments);
+    }
+
+    @GetMapping("/path")
+    public ResponseEntity<?> getCommentsByPath(
+            @PathVariable("review-id") Long reviewId,
+            @RequestParam("path") String path) {
+
+        List<ReviewCommentResponse> comments = reviewCommentService.getCommentsByReviewIdAndPath(
+                reviewId, path);
+        return ResponseEntity.ok(comments);
+    }
+
 //    @PostMapping("/upload")
 //    public ResponseEntity<String> uploadFile(
 //            @PathVariable("pr-id") Long prId,
