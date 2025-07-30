760aa662dbb200ec7016746a586f661f71753341
huizoo <ajtwlsgmlwn@naver.com>
2025-07-30 17:01:56 +0900

    fix: 로그인 후 새로고침하면 로그인이 해제되는 버그 수정

--- ottereview-fe/src/store/userStore.js
+++ ottereview-fe/src/store/userStore.js
@@ -1,7 +1,16 @@
 import { create } from 'zustand'
+import { persist } from 'zustand/middleware'
 
-export const useUserStore = create((set) => ({
-  isLoggedIn: false,
-  login: () => set({ isLoggedIn: true }),
-  logout: () => set({ isLoggedIn: false }),
-}))
+export const useUserStore = create(
+  persist(
+    (set) => ({
+      isLoggedIn: false,
+      login: () => set({ isLoggedIn: true }),
+      logout: () => set({ isLoggedIn: false }),
+    }),
+    {
+      name: 'user-storage',
+      getStorage: () => localStorage,
+    }
+  )
+)
