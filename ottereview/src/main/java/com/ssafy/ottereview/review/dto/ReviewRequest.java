5caa7a222aee10d88b1d2d7dd923f4167343c1ff
신진우 <sjw0066@naver.com>
2025-07-30 15:52:05 +0900

    [BE/REFACTOR] 코드 컨벤션 유지를 위한 수정

--- ottereview/src/main/java/com/ssafy/ottereview/review/dto/ReviewRequest.java
+++ ottereview/src/main/java/com/ssafy/ottereview/review/dto/ReviewRequest.java
@@ -3,24 +3,23 @@ package com.ssafy.ottereview.review.dto;
 import com.ssafy.ottereview.review.entity.ReviewState;
 import jakarta.validation.Valid;
 import jakarta.validation.constraints.NotNull;
+import java.util.List;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 
-import java.util.List;
-
 @Getter
-@NoArgsConstructor 
+@NoArgsConstructor
 @AllArgsConstructor
 public class ReviewRequest {
-    
+
     @NotNull
     private ReviewState state;
-    
+
     private String body;
-    
+
     private String commitSha;
-    
+
     @Valid
     private List<com.ssafy.ottereview.reviewcomment.dto.ReviewCommentCreateRequest.CommentItem> reviewComments;
 }
