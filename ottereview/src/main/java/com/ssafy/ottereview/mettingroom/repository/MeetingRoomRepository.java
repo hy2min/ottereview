a4a4d63557e61b8be63b43bea23a4842b094a8e2
박서진 <002seojin@gmail.com>
2025-07-31 13:43:08 +0900

    [BE/FEATURE] webRTC - 음성 채팅방 API 개발

--- ottereview/src/main/java/com/ssafy/ottereview/mettingroom/repository/MeetingRoomRepository.java
+++ ottereview/src/main/java/com/ssafy/ottereview/mettingroom/repository/MeetingRoomRepository.java
@@ -2,6 +2,7 @@ package com.ssafy.ottereview.mettingroom.repository;
 
 import com.ssafy.ottereview.mettingroom.entity.MeetingRoom;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Modifying;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
@@ -11,11 +12,12 @@ import java.util.Optional;
 @Repository
 public interface MeetingRoomRepository extends JpaRepository<MeetingRoom, Long> {
     @Query("""
-            SELECT DISTINCT r
-            FROM MeetingRoom r
-            LEFT JOIN FETCH r.participants p
+            SELECT DISTINCT m
+            FROM MeetingRoom m
+            LEFT JOIN FETCH m.participants p
             LEFT JOIN FETCH p.user
-            WHERE r.id = :roomId
+            WHERE m.id = :roomId
             """)
     Optional<MeetingRoom> findByIdWithParticipantsAndUsers(@Param("roomId") Long roomId);
+
 }
