f09aff11ab705730002c7e8d20680275155ce28a
윤경일 <kidreamx@naver.com>
2025-07-31 13:47:57 +0900

    [BE/FEATURE] 우선순위 테이블 crud 기능 개발

---
+++ ottereview/src/main/java/com/ssafy/ottereview/priority/controller/PriorityController.java
@@ -0,0 +1,36 @@
+package com.ssafy.ottereview.priority.controller;
+
+import com.ssafy.ottereview.priority.dto.CreatePriorityRequest;
+import com.ssafy.ottereview.priority.service.PriorityService;
+import java.util.List;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.ResponseEntity;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+@RequiredArgsConstructor
+@RequestMapping("/api/priority")
+public class PriorityController {
+
+    private final PriorityService priorityService;
+
+    /**
+     * pullRequestId를 가지고 해당하는 priorityList를 반환해준다.
+     * @param pullRequestId
+     * @return List<Priority>
+     */
+    @GetMapping("/{pr-id}")
+    public ResponseEntity<?> getPriorityByPriorityId(@PathVariable (name = "pr-id")Long pullRequestId){
+        return ResponseEntity.ok(priorityService.getPriorityListByPullRequest(pullRequestId));
+    }
+
+    @PostMapping()
+    public void createPriority(@RequestBody List<CreatePriorityRequest> createPriorityRequestList){
+        priorityService.createPriorityList(createPriorityRequestList);
+    }
+}
