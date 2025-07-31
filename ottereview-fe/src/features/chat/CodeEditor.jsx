eca378492436b21becc03b352889a8c3fab3f54c
이혜민 <hy1x1mn@gmail.com>
2025-07-31 16:49:42 +0900

    fix: 에러 상황 체크를 위한 console 확인

--- ottereview-fe/src/features/chat/CodeEditor.jsx
+++ ottereview-fe/src/features/chat/CodeEditor.jsx
@@ -1,6 +1,6 @@
+import * as Y from '@yorkie-js/sdk'
 import * as monaco from 'monaco-editor'
 import { useEffect, useRef } from 'react'
-import * as Y from 'yorkie-js-sdk'
 
 const CodeEditor = ({ roomId }) => {
   const editorRef = useRef(null)
@@ -10,43 +10,61 @@ const CodeEditor = ({ roomId }) => {
 
   useEffect(() => {
     const init = async () => {
-      // 1. Yorkie 클라이언트 생성 및 활성화
-      const client = new Y.client('http://localhost:8080')
-      await client.activate()
-      yorkieClientRef.current = client
+      try {
+        console.log('[Yorkie] Connecting client...')
 
-      //   2. 문서 생성 및 초기화
-      const doc = new Y.Document(`code-room-${roomId}`)
-      doc.update((root) => {
-        if (!root.code) root.code = new Y.text()
-      })
-      await client.attach(doc)
-      yorkieDocRef.current = doc
-
-      // 3. Monaco Editor 생성
-      const editor = monaco.editor.create(editorRef.current, {
-        value: doc.getRoot().code.toString(),
-        language: 'javascript',
-        theme: 'vs-dark',
-        automaticLayout: true,
-      })
-      monacoInstanceRef.current = editor
-
-      // 4. Monaco -> Yorkie 동기화
-      editor.onDidChangeModelContent(() => {
-        const newValue = editor.getValue()
-        doc.update((root) => {
-          root.code.edit(0, root.code.length, newValue)
+        // 1. Yorkie 클라이언트 생성 및 활성화
+        const client = new Y.Client({
+          rpcAddr: import.meta.env.VITE_YORKIE_API_ADDR,
+          apiKey: import.meta.env.VITE_YORKIE_API_KEY,
         })
-      })
-      // 5. Yorkie -> Monaco 반영
-      doc.getRoot().code.onChanges(() => {
-        const newVal = doc.getRoot().code.toString()
-        if (editor.getValue() !== newVal) {
-          editor.setValue(newVal)
-        }
-      })
+        await client.activate()
+        yorkieClientRef.current = client
+
+        console.log('[Yorkie] Client activated')
+
+        // 2. 문서 생성 및 초기화
+        const doc = new Y.Document(`code-room-${roomId}`)
+        await client.attach(doc)
+        console.log('[Yorkie] Document attached:', doc)
+        yorkieDocRef.current = doc
+        doc.update((root) => {
+          if (!root.code) {
+            root.code = new Y.Text()
+            root.code.insert(0, '//공유 코드를 입력하세요.')
+          }
+        })
+
+        // 3. Monaco Editor 생성
+        const editor = monaco.editor.create(editorRef.current, {
+          value: doc.getRoot().code.toString(),
+          language: 'javascript',
+          theme: 'vs-dark',
+          automaticLayout: true,
+        })
+        console.log('[Monaco] Editor intialized')
+        monacoInstanceRef.current = editor
+
+        // 4. Monaco -> Yorkie 동기화
+        editor.onDidChangeModelContent(() => {
+          const newValue = editor.getValue()
+          doc.update((root) => {
+            root.code.edit(0, root.code.length, newValue)
+          })
+        })
+
+        // 5. Yorkie -> Monaco 반영
+        doc.getRoot().code.onChanges(() => {
+          const newVal = doc.getRoot().code.toString()
+          if (editor.getValue() !== newVal) {
+            editor.setValue(newVal)
+          }
+        })
+      } catch (error) {
+        console.log('[ERROR]', error)
+      }
     }
+
     init()
 
     return () => {
