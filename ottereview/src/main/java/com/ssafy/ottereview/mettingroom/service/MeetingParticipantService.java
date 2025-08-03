8a41ba5f87403faa0fd0ada167e31abe424f892b
002seojin <002seojin@gmail.com>
2025-08-03 16:25:23 +0900

    chore: 백엔드 dev와 머지

--- ottereview/src/main/java/com/ssafy/ottereview/mettingroom/service/MeetingParticipantService.java
+++
@@ -1,9 +0,0 @@
-package com.ssafy.ottereview.mettingroom.service;
-
-import com.ssafy.ottereview.mettingroom.dto.MeetingParticipantDto;
-
-import java.util.List;
-
-public interface MeetingParticipantService {
-    List<MeetingParticipantDto> getParticipantsByRoomId(Long roomId);
-}
