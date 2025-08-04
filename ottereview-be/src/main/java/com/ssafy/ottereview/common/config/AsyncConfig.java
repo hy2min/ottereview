a769805ff84353eaf9af638f3532375d80048dac
박서진 <002seojin@gmail.com>
2025-08-04 13:43:08 +0900

    [BE/REFACTOR] 로그인 리다이렉트 구현

---
+++ ottereview-be/src/main/java/com/ssafy/ottereview/common/config/AsyncConfig.java
@@ -0,0 +1,10 @@
+package com.ssafy.ottereview.common.config;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.scheduling.annotation.EnableAsync;
+
+@Configuration
+@EnableAsync
+public class AsyncConfig {
+
+}
