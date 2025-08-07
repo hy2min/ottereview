235ebe3fa4f8385eb63d360207cb148e44a0a621
이혜민 <hy1x1mn@gmail.com>
2025-08-07 09:40:19 +0900

    fix: 레포 리스트 체크

--- ottereview-fe/src/features/repository/repoApi.js
+++ ottereview-fe/src/features/repository/repoApi.js
@@ -1,6 +1,6 @@
 import { api } from '@/lib/api'
 
-export const fetchRepoList = async (accountId) => {
-  const res = await api.get(`/api/accounts/${accountId}/repositories`)
+export const fetchRepoList = async () => {
+  const res = await api.get(`/api/accounts/1/repositories/users`)
   return res.data
 }
