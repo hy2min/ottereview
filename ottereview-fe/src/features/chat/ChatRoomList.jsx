d363286517337af59d3219a437c82d4acab712f2
박서진 <002seojin@gmail.com>
2025-08-09 22:45:46 +0900

    [BE/FIX] 시큐리티 설정과 화이트보드 로직

--- ottereview-fe/src/features/chat/ChatRoomList.jsx
+++ ottereview-fe/src/features/chat/ChatRoomList.jsx
@@ -1,8 +1,30 @@
+import { useEffect } from 'react'
+
+import { useAuthStore } from '@/features/auth/authStore'
+
+import { fetchChat } from './chatApi'
 import ChatRoomCard from './ChatRoomCard'
 import { useChatStore } from './chatStore'
 
 const ChatRoomList = () => {
   const rooms = useChatStore((state) => state.rooms)
+  const setRooms = useChatStore((state) => state.setRooms)
+  const accessToken = useAuthStore((state) => state.accessToken)
+
+  useEffect(() => {
+    const fetchRooms = async () => {
+      try {
+        const fetchedRooms = await fetchChat(accessToken)
+        setRooms(fetchedRooms)
+        console.log(fetchedRooms)
+      } catch (err) {
+        console.error('채팅방 목록 로딩 실패: ', err)
+      }
+    }
+    if (accessToken) {
+      fetchRooms()
+    }
+  }, [accessToken, setRooms])
 
   return (
     <div className="border p-2">
@@ -12,7 +34,7 @@ const ChatRoomList = () => {
       ) : (
         <div className="flex flex-wrap gap-2">
           {rooms.map((room) => (
-            <ChatRoomCard key={room.id} room={room} />
+            <ChatRoomCard key={room.roomId} room={room} />
           ))}
         </div>
       )}
--- ottereview-fe/src/features/chat/chatStore.js
+++ ottereview-fe/src/features/chat/chatStore.js
@@ -3,15 +3,21 @@ import { create } from 'zustand'
 // idCounter는 DB에 저장하면서 DB의 CHATROOM 번호와 연동할 것임.
 let idCounter = 1
 
-export const useChatStore = create((set, get) => ({
+export const useChatStore = create((set) => ({
   rooms: [],
-  addRoom: (roomData) => {
-    const newRoom = { id: idCounter++, createdAt: Date.now(), ...roomData }
+
+  setRooms: (fetchedRooms) =>
+    set(() => ({
+      rooms: fetchedRooms,
+    })),
+
+  addRoom: (roomData) =>
+    // const newRoom = { id: idCounter++, createdAt: Date.now(), ...roomData }
     set((state) => ({
-      rooms: [...state.rooms, newRoom],
-    }))
-    return newRoom.id
-  },
+      rooms: [...state.rooms, roomData],
+    })),
+  // return newRoom.id
+
   removeRoom: (id) =>
     set((state) => ({
       rooms: state.rooms.filter((room) => room.id !== id),
