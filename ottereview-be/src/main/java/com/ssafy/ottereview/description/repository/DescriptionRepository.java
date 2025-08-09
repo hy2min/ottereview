fd2b2a997aa61554eb16c7d5ab75b81ea4a0b951
신진우 <sjw0066@naver.com>
2025-08-09 18:40:39 +0900

    [BE/FEATURE] 디스크립션 생성 CRUD/ pull request 생성시 되게 묶기

--- ottereview-be/src/main/java/com/ssafy/ottereview/description/repository/DescriptionRepository.java
+++ ottereview-be/src/main/java/com/ssafy/ottereview/description/repository/DescriptionRepository.java
@@ -1,10 +1,13 @@
 package com.ssafy.ottereview.description.repository;
 
 import com.ssafy.ottereview.description.entity.Description;
+import java.util.List;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
 @Repository
 public interface DescriptionRepository extends JpaRepository<Description, Long> {
+
+    List<Description> findByPullRequestId(Long pullRequestId);
 
 }
--- ottereview-be/src/main/java/com/ssafy/ottereview/pullrequest/service/PullRequestService.java
+++ ottereview-be/src/main/java/com/ssafy/ottereview/pullrequest/service/PullRequestService.java
@@ -6,6 +6,7 @@ import com.ssafy.ottereview.pullrequest.dto.response.PullRequestResponse;
 import com.ssafy.ottereview.user.entity.CustomUserDetail;
 import java.util.List;
 import org.kohsuke.github.GHRepository;
+import org.springframework.web.multipart.MultipartFile;
 
 public interface PullRequestService {
     
@@ -35,11 +36,17 @@ public interface PullRequestService {
     PullRequestResponse getPullRequestByBranch(CustomUserDetail userDetail, Long repoId, String source, String target);
     
     /**
-     *  새로운 Pull Request를 생성합니다.
+     *  미디어 파일과 함께 새로운 Pull Request를 생성합니다.
+     *  
+     *  네이밍 명확화:
+     *  - Git 파일 변경 정보는 GitHub API로 가져옴
+     *  - mediaFiles 파라미터 -> 업로드할 미디어 파일들 (MultipartFile[])
      *
-     * @param pullRequestCreateRequest Pull Request 생성 요청 정보
+     * @param pullRequestCreateRequest Pull Request 생성 요청 정보 (source, target 브랜치)
+     * @param mediaFiles 업로드할 미디어 파일 배열 (음성, 이미지 등의 설명 첨부파일)
      */
-    void createPullRequest(CustomUserDetail customUserDetail, Long repoId, PullRequestCreateRequest pullRequestCreateRequest);
+    void createPullRequestWithMediaFiles(CustomUserDetail customUserDetail, Long repoId, 
+                                       PullRequestCreateRequest pullRequestCreateRequest, MultipartFile[] mediaFiles);
     
     /**
      * Github에 있는 PullRequest를 DB에 저장합니다.
