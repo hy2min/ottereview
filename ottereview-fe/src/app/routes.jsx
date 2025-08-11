3b80847b6b22c75f3a72bfb9ebe6b3a4118274d7
신진우 <sjw0066@naver.com>
2025-08-11 17:59:46 +0900

    feat: 가이드 페이지

--- ottereview-fe/src/app/routes.jsx
+++ ottereview-fe/src/app/routes.jsx
@@ -3,12 +3,14 @@ import Whiteboard from '@/features/webrtc/Whiteboard'
 import ChatRoom from '@/pages/ChatRoom'
 import Conflict from '@/pages/Conflict'
 import Dashboard from '@/pages/Dashboard'
+import Guide from '@/pages/Guide'
 import PRCreate from '@/pages/PRCreate'
 import PRreview from '@/pages/PRReview'
 import RepositoryDetail from '@/pages/RepositoryDetail'
 
 export const protectedRoutes = [
   { path: '/dashboard', element: <Dashboard />, title: '대시보드' },
+  { path: '/guide', element: <Guide />, title: '사용 가이드' },
   { path: '/:repoId', element: <RepositoryDetail />, title: '레포지토리 상세' },
   { path: '/:repoId/pr/:prId/review', element: <PRreview />, title: 'PR 리뷰' },
   { path: '/:repoId/pr/:prId/conflict', element: <Conflict />, title: '충돌 해결' },
