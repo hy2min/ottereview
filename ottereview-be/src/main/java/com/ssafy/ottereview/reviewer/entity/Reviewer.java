f8bec3d701cc9dd0ebcb63b30405c18b2579ca94
박서진 <002seojin@gmail.com>
2025-08-11 11:54:16 +0900

    feature: reviewer 상태 엔티티 추가

--- ottereview-be/src/main/java/com/ssafy/ottereview/reviewer/entity/Reviewer.java
+++ ottereview-be/src/main/java/com/ssafy/ottereview/reviewer/entity/Reviewer.java
@@ -2,12 +2,7 @@ package com.ssafy.ottereview.reviewer.entity;
 
 import com.ssafy.ottereview.pullrequest.entity.PullRequest;
 import com.ssafy.ottereview.user.entity.User;
-import jakarta.persistence.Entity;
-import jakarta.persistence.GeneratedValue;
-import jakarta.persistence.GenerationType;
-import jakarta.persistence.Id;
-import jakarta.persistence.JoinColumn;
-import jakarta.persistence.ManyToOne;
+import jakarta.persistence.*;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Getter;
@@ -35,5 +30,10 @@ public class Reviewer {
     @JoinColumn(name = "user_id", nullable = false)
     @OnDelete(action = OnDeleteAction.CASCADE)
     private User user;
+
+    @Enumerated(EnumType.STRING)
+    @Column(nullable = false)
+    private ReviewStatus status;
+
 
 }
