5aaa28b9d4495419a04b493ca28044e6ea90c9d7
윤경일 <kidreamx@naver.com>
2025-08-13 22:26:12 +0900

    [BE/FEATURE] cursor 기반 페이지네이션 코드 추가

--- ottereview-be/src/main/java/com/ssafy/ottereview/pullrequest/repository/PullRequestRepository.java
+++ ottereview-be/src/main/java/com/ssafy/ottereview/pullrequest/repository/PullRequestRepository.java
@@ -4,12 +4,16 @@ import com.ssafy.ottereview.pullrequest.entity.PrState;
 import com.ssafy.ottereview.pullrequest.entity.PullRequest;
 import com.ssafy.ottereview.repo.entity.Repo;
 import com.ssafy.ottereview.user.entity.User;
+
+import java.time.Instant;
 import java.util.List;
 import java.util.Optional;
 
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
 
 @Repository
@@ -26,4 +30,17 @@ public interface PullRequestRepository extends JpaRepository<PullRequest, Long> 
     Optional<PullRequest> findByRepoAndBaseAndHeadAndState(Repo repo, String base, String head, PrState state);
     
     Boolean existsByRepoAndBaseAndHeadAndState(Repo repo, String base, String head, PrState state);
+
+    @Query("""
+  SELECT p FROM PullRequest p
+  WHERE p.repo.repoId = :repoId
+    AND (:cursorUpdatedAt IS NULL OR p.githubUpdatedAt < :cursorUpdatedAt
+         OR (p.githubUpdatedAt= :cursorUpdatedAt AND p.id < :id))
+  ORDER BY p.githubUpdatedAt DESC, p.id DESC
+""")
+    List<PullRequest> findSlice(
+            @Param("repo") Repo repo,
+            @Param("cursorUpdatedAt") Instant cursorUpdatedAt,
+            @Param("cursorId") Long cursorId,
+            Pageable pageable);
 }
