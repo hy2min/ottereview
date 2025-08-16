f512769335727aa63cb830f37f36e30ee720c3b3
이혜민 <hy1x1mn@gmail.com>
2025-08-16 22:42:11 +0900

    style: 컨벤션 규칙 표현방식 수정

--- ottereview-fe/src/features/pullRequest/PRCreateStep1.jsx
+++ ottereview-fe/src/features/pullRequest/PRCreateStep1.jsx
@@ -1,6 +1,6 @@
+import { AlertTriangle, CheckCircle, FileText, Search, X } from 'lucide-react'
 import { useEffect, useState } from 'react'
 import { useNavigate } from 'react-router-dom'
-import { AlertTriangle, CheckCircle, X, FileText, Search } from 'lucide-react'
 
 import Box from '@/components/Box'
 import Button from '@/components/Button'
